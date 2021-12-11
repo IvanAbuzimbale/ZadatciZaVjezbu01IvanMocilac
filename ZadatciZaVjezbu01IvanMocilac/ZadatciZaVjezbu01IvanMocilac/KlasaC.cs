1c88a807328cefd3c1afc2551c0fe035bc83e64b
IvanAbuzimbale <58472137+IvanAbuzimbale@users.noreply.github.com>
2021-12-11 17:58:16 +0100

    Add files via upload

---
+++ ZadatciZaVjezbu01IvanMocilac/ZadatciZaVjezbu01IvanMocilac/KlasaC.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZadatciZaVjezbu01IvanMocilac
+{
+    class KlasaC
+    {
+        public int Zbroj(int[] niz)
+        {
+            int suma = 0;
+            for (int i = 0; i < niz.Length; i++)
+            {
+                suma = suma + niz[i];
+            }
+            return suma;
+        }
+
+        public double Prosjek(int[] niz)
+        {
+            int suma = 0;
+            for (int i = 0; i < niz.Length; i++)
+            {
+                suma = suma + niz[i];
+            }
+            return (double)suma / niz.Length;
+        }
+    }
+}
---
+++ ZadatciZaVjezbu01IvanMocilac/ZadatciZaVjezbu01IvanMocilac/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZadatciZaVjezbu01IvanMocilac
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] niz = new int[5];
+
+            Console.WriteLine("Upišite 5 cijelih brojeva");
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write("Upišite " + (i+1) + ". broj: ");
+                string s = Console.ReadLine();
+                niz[i] = Convert.ToInt32(s);
+            }
+
+            KlasaC c = new KlasaC();
+            int zbroj = c.Zbroj(niz);
+            Console.WriteLine("Zbroj zadanih brojeva je: " + zbroj);
+            double prosjek = c.Prosjek(niz);
+            Console.WriteLine("Prosjek zadanih projeva je: " + prosjek);
+            Console.ReadKey();
+        }
+    }
+}
